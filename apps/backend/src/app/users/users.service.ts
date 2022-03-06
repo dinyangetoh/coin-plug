8dbae2ff720f27a382d09e7a6bd881da1edfb397
David Inyangetoh <dinyangetoh@gmail.com>
2022-03-06 19:18:13 +0100

    add mongodb support

--- apps/backend/src/app/users/users.service.ts
+++ apps/backend/src/app/users/users.service.ts
@@ -1,20 +1,28 @@
 import {Injectable} from '@nestjs/common'
-import {User} from "./interfaces/user.interface";
+// import {User} from "./interfaces/user.interface";
+import {Model} from 'mongoose'
+import {InjectModel} from "@nestjs/mongoose";
+import {User, UserDocument} from "./schemas/user.schema";
+import {CreateUserDto} from "./dto/create-user.dto";
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+  }
+
   private readonly users: User[] = [
     {name: 'David Inyang-Etoh', age: 26},
     {name: 'James Brown', age: 16},
     {name: "Ekpe Ikot", age: 21}
   ];
 
-  create(user: User) {
-    this.users.push(user)
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const createUser = new this.userModel(createUserDto)
+    return createUser.save();
   }
 
-  findAll(): User[] {
-    return this.users;
+  findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
   }
 
 }
