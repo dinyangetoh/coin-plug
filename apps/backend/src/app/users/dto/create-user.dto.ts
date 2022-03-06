9ea3dbe0ba51ab02ba066abb0fa048f26b670abc
David Inyangetoh <dinyangetoh@gmail.com>
2022-03-06 18:44:19 +0100

    add validation support for form inputs

--- apps/backend/src/app/users/dto/create-user.dto.ts
+++ apps/backend/src/app/users/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
+
 export class CreateUserDto {
+  @IsNotEmpty() @IsString()
   name: string;
+  @IsNotEmpty() @IsNumber()
   age: number;
 }
