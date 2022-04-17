faf43c8981f8099b75c7d5e78184ea038c15f42d
David Inyangetoh <dinyangetoh@gmail.com>
2022-04-17 13:43:49 +0100

    add support for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./apps/frontend/pages/**/*.{js,ts,jsx,tsx}",
+    "./apps/frontend/src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
